f0279c14eb0f121926b0019959a753ff8a70083b
Adelina <54507003+alex-svg@users.noreply.github.com>
2020-06-19 16:46:15 +0100

    update server

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, console.log(`server is listening to ${PORT}`));
