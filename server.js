b30283ea4d6c3dcd99c22c585a0573e1bbbc25bd
Adelina <54507003+alex-svg@users.noreply.github.com>
2020-06-22 13:04:44 +0100

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require("./routes/api");
 //const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
-const MONGDB_URI = require("./config/keys");
+const db = require("./config/keys").MONGDB_URI;
 require("dotenv").config();
 
 console.log(process.env);
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(MONGDB_URI, {
+  .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
